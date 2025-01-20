81e3e85d960de9d20aa505d869955cc6c66ad4d5
Trevor Cickovski <tcickovs@delphi.cs.fiu.edu>
2025-01-20 15:18:16 -0500

    Removed sorting, switched to view

--- SAM2BAMPlugin.cpp
+++ SAM2BAMPlugin.cpp
@@ -13,8 +13,9 @@ void SAM2BAMPlugin::run() {
 
 void SAM2BAMPlugin::output(std::string file) {
  // alpha_diversity.py -i filtered_otu_table.biom -m observed_species,chao1,shannon,PD_whole_tree -t rep_set.tre -o alpha.txt
-   std::string command = "samtools sort " + inputfile + " > "+file;
- std::cout << command << std::endl;
+   //std::string command = "samtools sort " + inputfile + " > "+file;
+	std::string command = "samtools view -Sb " + inputfile + " -o " + file;
+	std::cout << command << std::endl;
 
  system(command.c_str());
 }
